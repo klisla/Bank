9966c8b334e9e8b154e65fd24535baf55bcb86a1
Fritz <fritz@spengergasse.at>
2020-06-15 08:15:02 +0200

    Credit Limit as INT

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,6 +3,15 @@ public class BankAccount {
 	private int accountId;
 	private String name;
 	private double amount;
+	private int creditLimit;
+	
+	
+	public int getCreditLimit() {
+		return creditLimit;
+	}
+	public void setCreditLimit(int creditLimit) {
+		this.creditLimit = creditLimit;
+	}
 	public int getAccountId() {
 		return accountId;
 	}
